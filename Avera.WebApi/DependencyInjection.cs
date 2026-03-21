b2a26cc100c9055aa80508c2af874cff80faabc2
recca383 <sirpatrick121402@gmail.com>
2026-03-21 23:42:34 +0800

    Added Dependency injections files in each project

---
+++ Avera.WebApi/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Avera.WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebApi(IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
