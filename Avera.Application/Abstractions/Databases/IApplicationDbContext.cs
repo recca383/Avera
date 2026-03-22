41339610717d894736ac3ddd6289db9355eecb05
recca383 <sirpatrick121402@gmail.com>
2026-03-22 23:46:43 +0800

    Added ApplicationDbContext and IdentityDbContext abstractions

---
+++ Avera.Application/Abstractions/Databases/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Application.Abstractions.Databases
+{
+    public interface IApplicationDbContext;
+}
