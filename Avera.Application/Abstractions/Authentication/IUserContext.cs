48656feff632469f1dd05261763e01d0e7820789
recca383 <sirpatrick121402@gmail.com>
2026-03-22 23:44:52 +0800

    Added UserContext as Abstraction to authentication

---
+++ Avera.Application/Abstractions/Authentication/IUserContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Application.Abstractions.Authentication
+{
+    public interface IUserContext
+    {
+        Guid UserId { get; }
+        Guid TenantId { get; }
+    }
+}
