ffe587049f0dc2ec8d6515ef68586bc374a03439
recca383 <sirpatrick121402@gmail.com>
2026-03-26 23:57:08 +0800

    Added Refresh Token endpoint template

---
+++ Avera.WebApi/Endpoints/Auth/Refresh.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Avera.WebApi.Endpoints.Auth
+{
+    internal sealed class Refresh : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapPost("/auth/refresh", () =>
+            {
+
+            });
+        }
+    }
+}
