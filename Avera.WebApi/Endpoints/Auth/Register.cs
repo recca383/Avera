4d5b9f6bfb741fe6ac4e51f86f411a3818d706d8
recca383 <sirpatrick121402@gmail.com>
2026-03-27 23:57:29 +0800

    Added Register endpoint template

---
+++ Avera.WebApi/Endpoints/Auth/Register.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Avera.WebApi.Endpoints.Auth
+{
+    internal sealed class Register : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapPost("/auth/register", () =>
+            {
+
+            });
+        }
+    }
+}
