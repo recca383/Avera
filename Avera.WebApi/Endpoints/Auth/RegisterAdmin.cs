ba739c1f941efc706f6129234d0a9d6c0cf02668
recca383 <sirpatrick121402@gmail.com>
2026-03-28 23:57:55 +0800

    Added Register Admin endpoint template

---
+++ Avera.WebApi/Endpoints/Auth/RegisterAdmin.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Avera.WebApi.Endpoints.Auth
+{
+    internal sealed class RegisterAdmin : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapPost("/auth/register-admin", () =>
+            {
+
+            });
+        }
+    }
+}
