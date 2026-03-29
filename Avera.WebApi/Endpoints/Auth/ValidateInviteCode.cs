be5e249306b86356e4cd73508fda30996ef1774e
recca383 <sirpatrick121402@gmail.com>
2026-03-29 23:59:26 +0800

    Added Validate Invite Code endpoint template

---
+++ Avera.WebApi/Endpoints/Auth/ValidateInviteCode.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Avera.WebApi.Endpoints.Auth
+{
+    internal sealed class ValidateInviteCode : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapGet("/auth/validate-invite-code/{code}",()=>
+            {
+
+            });
+        }
+    }
+}
