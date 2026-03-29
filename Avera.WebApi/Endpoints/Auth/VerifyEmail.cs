45a1664f731bc129e57bf3d27e4c5a3871d46ca6
recca383 <sirpatrick121402@gmail.com>
2026-03-30 00:00:16 +0800

    Added Verify email endpoint template

---
+++ Avera.WebApi/Endpoints/Auth/VerifyEmail.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Avera.WebApi.Endpoints.Auth
+{
+    internal sealed class VerifyEmail : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapPost("/auth/verify-email", ()=>
+            {
+
+            });
+        }
+    }
+}
