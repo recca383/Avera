f45de03b51e50d65b6ac1a25f9074f00586ded79
recca383 <sirpatrick121402@gmail.com>
2026-04-03 19:51:17 +0800

    Created Datetime Provider implementation

---
+++ Avera.Infrastructure/Time/DateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Infrastructure.Time
+{
+    internal sealed class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
