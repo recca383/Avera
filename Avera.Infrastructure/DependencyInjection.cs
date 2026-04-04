28354dab448a66a60e56364cfd4875c3dbb15275
recca383 <sirpatrick121402@gmail.com>
2026-04-04 19:52:49 +0800

    Completed Infrastructure dependency injection skeleton

--- Avera.Infrastructure/DependencyInjection.cs
+++ Avera.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Avera.Infrastructure.Time;
+using Infrastructure.DomainEvents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel;
 
 namespace Avera.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddServices()
+            .AddDatabase(configuration)
+            .AddHealthChecks(configuration)
+            .AddAuthenticationInternal(configuration)
+            .AddAuthorizationInternal();
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+            services.AddTransient<IDomainEventsDispatcher, DomainEventsDispatcher>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+
+            return services;
+        }
+
+        private static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
         {
             return services;
         }
+
+        private static IServiceCollection AddAuthenticationInternal(this IServiceCollection services, IConfiguration configuration)
+        {
+
+
+            return services;
+        }
+
+        private static IServiceCollection AddAuthorizationInternal(this IServiceCollection services)
+        { 
+            return services;
+        }
     }
+
+
 }
