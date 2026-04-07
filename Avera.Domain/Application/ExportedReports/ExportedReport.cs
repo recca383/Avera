5a753665d5c151aebb653e2a202428b9289640a2
recca383 <sirpatrick121402@gmail.com>
2026-04-07 19:56:22 +0800

    Added Exported Report Entity on Domain

---
+++ Avera.Domain/Application/ExportedReports/ExportedReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Domain.Application.ExportedReports
+{
+    public sealed class ExportedReport
+    {
+        public Guid Id { get; set; }
+        public Guid CaseId { get; set; }
+        public string FileUrl { get; set; }
+        public string StorageKey { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
