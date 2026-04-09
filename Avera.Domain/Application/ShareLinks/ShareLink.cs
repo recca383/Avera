0cd95912a7c1c56e1ed1eb83a9151712a100a898
recca383 <sirpatrick121402@gmail.com>
2026-04-09 19:57:19 +0800

    Added ShareLinks Entity on Domain

---
+++ Avera.Domain/Application/ShareLinks/ShareLink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Domain.Application.ShareLinks
+{
+    public sealed class ShareLink
+    {
+        public Guid Id { get; set; }
+        public Guid CaseId { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
