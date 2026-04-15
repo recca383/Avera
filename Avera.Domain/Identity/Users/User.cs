026e411e5a0ea4276b73f49bacb124b21d57d850
recca383 <sirpatrick121402@gmail.com>
2026-04-15 19:58:27 +0800

    Added User Identity Entity on Domain

---
+++ Avera.Domain/Identity/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Domain.Identity.Users
+{
+    public class User : IdentityUser
+    {
+        // Navigation 
+        public Guid TenantId { get; set; }
+    }
+}
