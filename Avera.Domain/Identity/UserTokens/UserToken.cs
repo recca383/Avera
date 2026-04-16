64a011a6d0809581dee2986027f775326e411cc4
recca383 <sirpatrick121402@gmail.com>
2026-04-16 19:58:40 +0800

    Added User Token Identity Entity on Domain

---
+++ Avera.Domain/Identity/UserTokens/UserToken.cs
@@ -0,0 +1,14 @@
+﻿using Avera.Domain.Identity.Users;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avera.Domain.Identity.UserTokens
+{
+    public sealed class UserToken : IdentityUserToken<Guid>
+    {
+    }
+}
