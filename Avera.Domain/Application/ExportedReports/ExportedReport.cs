c147cf21f14195dfa10b38cc16647fde02d399cf
recca383 <sirpatrick121402@gmail.com>
2026-04-19 11:13:49 +0800

    Added Navigation properties to exported report entity

--- Avera.Domain/Application/ExportedReports/ExportedReport.cs
+++ Avera.Domain/Application/ExportedReports/ExportedReport.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avera.Domain.Application.Cases;
 
 namespace Avera.Domain.Application.ExportedReports
 {
     public sealed class ExportedReport
     {
         public Guid Id { get; set; }
-        public Guid CaseId { get; set; }
-        public string FileUrl { get; set; }
-        public string StorageKey { get; set; }
+        public string? FileUrl { get; set; }
+        public string? StorageKey { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        // Navigation Properties
+        public Guid CaseId { get; set; }
+        public Case? Case { get; set; }
     }
 }
