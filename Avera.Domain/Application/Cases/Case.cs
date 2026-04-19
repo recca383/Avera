cc1a54ae0f81a2e3625e78a062ca7d83484f23cf
recca383 <sirpatrick121402@gmail.com>
2026-04-19 11:13:29 +0800

    Added Navigation properties to Case entity

--- Avera.Domain/Application/Cases/Case.cs
+++ Avera.Domain/Application/Cases/Case.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avera.Domain.Application.CaseImages;
+using Avera.Domain.Application.ExportedReports;
+using Avera.Domain.Identity.Users;
 
 namespace Avera.Domain.Application.Cases
 {
     public sealed class Case
     {
         public Guid Id { get; set; }
-        public string CaseCode { get; set; }
-        public string SubjectName { get; set; }
+        public string CaseCode { get; set; } = string.Empty;
+        public string SubjectName { get; set; } = "No Subject";
         public DocumentType Type { get; set; }
         public Priority Priority { get; set; }
-        public string Notes { get; set; }
+        public string Notes { get; set; } = string.Empty;
         public Status Status { get; set; }
         public Verdict Verdict { get; set; }
         public DateTime DeletedAt { get; set; }
-        public Guid UserId { get; set; }
-        public Guid OrganizationId { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        // Navigation Properties
+        public User? User { get; set; }
+        public Guid UserId { get; set; }
+        public List<CaseImage> CaseImages { get; set; } = new();
+        public List<ExportedReport> ExportedReports { get; set; } = new();
     }
 }
