c62b02d1f20fa260964d1b9e5a2c7dd1038bcb62
recca383 <sirpatrick121402@gmail.com>
2026-04-20 11:14:21 +0800

    Moved sharelink entity to Identity folder

--- Avera.Domain/Identity/ShareLinks/ShareLink.cs
+++ Avera.Domain/Identity/ShareLinks/ShareLink.cs
@@ -4,15 +4,20 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avera.Domain.Identity.Tenants;
 
-namespace Avera.Domain.Application.ShareLinks
+namespace Avera.Domain.Identity.ShareLinks
 {
     public sealed class ShareLink
     {
         public Guid Id { get; set; }
-        public Guid CaseId { get; set; }
-        public string Token { get; set; }
+        public string Token { get; set; } = string.Empty;
         public DateTime ExpiresAt { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        // Navigation Properties
+        public Guid TenantId { get; set; }
+        public Tenant? Tenant { get; set; }
+
     }
 }
