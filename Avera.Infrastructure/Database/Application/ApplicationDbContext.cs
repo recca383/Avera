cc4ef8fdf36d00d77c79548b08b7a90cb7b4e6c5
recca383 <sirpatrick121402@gmail.com>
2026-04-23 11:19:32 +0800

    Completed ApplicationDBContext implementation

--- Avera.Infrastructure/Database/Application/ApplicationDbContext.cs
+++ Avera.Infrastructure/Database/Application/ApplicationDbContext.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avera.Application.Abstractions.Databases;
+using Avera.Domain.Application.CaseImages;
+using Avera.Domain.Application.Cases;
+using Avera.Domain.Application.ExportedReports;
+using Avera.Domain.Application.Notifications;
+using Avera.Domain.Identity.ShareLinks;
+using Infrastructure.Database;
+using Infrastructure.DomainEvents;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
 
 namespace Avera.Infrastructure.Database.Application
 {
-    internal class ApplicationDbContext
+    internal class ApplicationDbContext(DbContextOptions options,
+     IDomainEventsDispatcher domainEventsDispatcher)
+    : DbContext(options), IApplicationDbContext
     {
+        public DbSet<Case> Cases { get; set; }
+        public DbSet<CaseImage> CaseImages { get; set; }
+        public DbSet<ExportedReport> ExportedReports { get; set; }
+        public DbSet<ShareLink> ShareLinks { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            
+            modelBuilder.HasDefaultSchema(Schemas.Default);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            int result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            return result;
+        }
+
+        private async Task PublishDomainEventsAsync()
+        {
+            var domainEvents = ChangeTracker
+                .Entries<Entity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity =>
+                {
+                    List<IDomainEvent> domainEvents = entity.DomainEvents;
+
+                    entity.ClearDomainEvents();
+
+                    return domainEvents;
+                })
+                .ToList();
+
+            await domainEventsDispatcher.DispatchAsync(domainEvents);
+        }
     }
 }
