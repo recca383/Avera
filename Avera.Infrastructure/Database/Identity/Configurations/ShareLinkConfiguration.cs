dc51de8967f0bbfafb9647a65c7c574565b18f1c
recca383 <sirpatrick121402@gmail.com>
2026-04-25 23:23:25 +0800

    Finalized ShareLink Entity Configuration

---
+++ Avera.Infrastructure/Database/Identity/Configurations/ShareLinkConfiguration.cs
@@ -0,0 +1,19 @@
+using Avera.Domain.Identity.ShareLinks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Avera.Infrastructure.Database.Identity.Configurations
+{
+    internal class ShareLinkConfiguration : IEntityTypeConfiguration<ShareLink>
+    {
+        public void Configure(EntityTypeBuilder<ShareLink> builder)
+        {
+            builder.HasKey(sl => sl.Id);
+
+            builder.HasIndex(sl => sl.Id);
+            builder.HasIndex(sl => sl.CreatedAt);
+            builder.HasIndex(sl => sl.TenantId);
+
+        }
+    }
+}
