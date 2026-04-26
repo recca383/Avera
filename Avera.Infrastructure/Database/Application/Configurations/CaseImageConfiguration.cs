c4f0d58800eb4f210827c532e0a2f1be2f7d3272
recca383 <sirpatrick121402@gmail.com>
2026-04-26 23:23:44 +0800

    Finalized Case Image entity Configuration

---
+++ Avera.Infrastructure/Database/Application/Configurations/CaseImageConfiguration.cs
@@ -0,0 +1,22 @@
+using Avera.Domain.Application.CaseImages;
+using Avera.Domain.Application.Cases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Avera.Infrastructure.Database.Application.Configurations
+{
+    internal sealed class CaseConfigurations : IEntityTypeConfiguration<CaseImage>
+    {
+        public void Configure(EntityTypeBuilder<CaseImage> builder)
+        {
+            builder.HasKey(ci => ci.Id);
+            
+            builder.HasIndex(ci => ci.CaseId);
+            builder.HasIndex(ci => ci.CreatedAt);
+            
+            builder.HasOne<Case>(ci => ci.Case)
+                .WithMany(c => c.CaseImages)
+                .HasForeignKey(ci => ci.CaseId);
+        }
+    }
+}
