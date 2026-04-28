2d2e41d53899d71b8171cb2d2b82788598ab1764
recca383 <sirpatrick121402@gmail.com>
2026-04-28 23:23:10 +0800

    Finalized Exported Report Entity Configuration

---
+++ Avera.Infrastructure/Database/Application/Configurations/ExportedReportConfiguration.cs
@@ -0,0 +1,21 @@
+using Avera.Domain.Application.ExportedReports;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Avera.Infrastructure.Database.Application.Configurations
+{
+    internal class ExportedReportConfiguration : IEntityTypeConfiguration<ExportedReport>
+    {
+        public void Configure(EntityTypeBuilder<ExportedReport> builder)
+        {
+            builder.HasKey(er => er.Id);
+
+            builder.HasIndex(er => er.CaseId);
+            builder.HasIndex(er => er.CreatedAt);
+
+            builder.HasOne(er => er.Case)
+                .WithMany(c => c.ExportedReports)
+                .HasForeignKey(er => er.CaseId);
+        }
+    }
+}
