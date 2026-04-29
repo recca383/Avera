763d6cd6d294fec93fb630ec8cda9299192a67ad
recca383 <sirpatrick121402@gmail.com>
2026-04-29 23:23:32 +0800

    Renamed IIdentityDbContext to IApplicationIdentityDbContext

---
+++ Avera.Application/Abstractions/Databases/IApplicationIdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avera.Domain.Identity.SubscriptionPlans;
+using Avera.Domain.Identity.Tenants;
+using Avera.Domain.Identity.TenantSubscriptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avera.Application.Abstractions.Databases
+{
+    public interface IApplicationIdentityDbContext
+    {
+
+        DbSet<Tenant> Tenants { get; set; }
+        DbSet<TenantSubscription> TenantSubscriptions {get; set; }
+        DbSet<SubscriptionPlan> SubscriptionPlans { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
