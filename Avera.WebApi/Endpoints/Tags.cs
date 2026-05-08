67301813f12b580c14c30da040e15b11dcf487b9
recca383 <sirpatrick121402@gmail.com>
2026-05-08 10:34:19 +0800

    Added Tags static class for endpoints

---
+++ Avera.WebApi/Endpoints/Tags.cs
@@ -0,0 +1,17 @@
+namespace Avera.WebApi.Endpoints
+{
+    public static class Tags
+    {
+        public static string Auth = "Authentications";
+        public static string Cases = "Cases";
+        public static string Export = "Exported Files";
+        public static string ML = "ML Inferences";
+        public static string Notifications = "Notifications";
+        public static string OrgAdmin = "Org Admin";
+        public static string Profile = "Profile";
+        public static string Stats = "Statistics";
+        public static string SuperAdmin = "Super Admin";
+        
+            
+    }
+}
