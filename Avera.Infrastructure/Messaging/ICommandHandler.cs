3183bd00e8f97d424ca7eb20b6b462b4b872dd58
recca383 <sirpatrick121402@gmail.com>
2026-05-12 11:58:52 +0800

    Created Command and CommandHandler interface for Messaging

---
+++ Avera.Infrastructure/Messaging/ICommandHandler.cs
@@ -0,0 +1,14 @@
+using SharedKernel;
+
+namespace Avera.Application.Messaging
+{
+    public interface ICommandHandler<in TCommand> where TCommand : ICommand
+    {
+        Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
+    }
+
+    public interface ICommandHandler<in TCommand, TResponse> where TCommand : ICommand<TResponse>
+    {
+        Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
+    }
+}
