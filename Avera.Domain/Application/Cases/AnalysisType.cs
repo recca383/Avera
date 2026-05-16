8cbb5dd91b09d15da4c370b80a95438e24fb4adc
recca383 <sirpatrick121402@gmail.com>
2026-05-16 22:50:32 +0800

    Add AnalysisType enum and remove unused DocumentType and endpoint classes

---
+++ Avera.Domain/Application/Cases/AnalysisType.cs
@@ -0,0 +1,9 @@
+﻿namespace Avera.Domain.Application.Cases
+{
+    public enum AnalysisType
+    {
+        SIG, //Signature
+        HW, //Handwritten
+        DOC  //Document
+    }
+}
--- Avera.Domain/Application/Cases/DocumentType.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Avera.Domain.Application.Cases
-{
-    public enum DocumentType
-    {
-    }
-}
