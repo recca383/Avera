6915c5fd906e52f7889ec15832d986853d97b0d3
recca383 <sirpatrick121402@gmail.com>
2026-05-27 14:30:38 +0800

    Enhance CreateCaseCommandHandler to check for existing case codes and log success message upon case creation

--- Avera.Application/Cases/Create/CreateCaseCommandHandler.cs
+++ Avera.Application/Cases/Create/CreateCaseCommandHandler.cs
@@ -1,6 +1,7 @@
 using Avera.Application.Abstractions.Databases;
 using Avera.Application.Abstractions.Messaging;
 using Avera.Domain.Application.Cases;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SharedKernel;
 
@@ -26,13 +27,22 @@ namespace Avera.Application.Cases.Create
                 Status = Status.Processing
             };         
 
+            if (await dbContext.Cases.AnyAsync(c => c.CaseCode == newCase.CaseCode, cancellationToken))
+            {
+                logger.LogWarning("A case with code: {CaseCode} already exists", newCase.CaseCode);
+                return Result.Failure(CaseErrors.CaseAlreadyExists);
+            }
+            
             logger.LogInformation("Adding new case to database: {CaseCode}", newCase.CaseCode);
+
             await dbContext.Cases.AddAsync(newCase, cancellationToken);
+
             logger.LogInformation("New case added to database: {CaseCode}", newCase.CaseCode);
 
             await dbContext.SaveChangesAsync(cancellationToken);
 
             logger.LogInformation("Case created successfully with ID: {CaseId}", newCase.Id);
+            
             return Result.Success();
         }
 
