625ae3e556d3c667781fc260917fbc86ac269d20
recca383 <sirpatrick121402@gmail.com>
2026-05-28 14:30:43 +0800

    Implement PatchStatus endpoint to handle status updates for cases

--- Avera.WebApi/Endpoints/Cases/PatchStatus.cs
+++ Avera.WebApi/Endpoints/Cases/PatchStatus.cs
@@ -1,13 +1,31 @@
 
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.PatchStatus;
+using Avera.Domain.Application.Cases;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
 namespace Avera.WebApi.Endpoints.Cases
 {
     internal sealed class PatchStatus : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPatch("cases/{id:guid}/status", ()=>
+            routeBuilder.MapPatch("cases/{id:guid}/status", async (
+                Guid id, 
+                Status status, 
+                ICommandHandler<PatchStatusCaseCommand, Guid> handler,
+                CancellationToken cancellationToken) =>
             {
+                var command = new PatchStatusCaseCommand
+                {
+                    Id = id,
+                    Status = status
+                };
+
+                var result = await handler.Handle(command, cancellationToken);
                 
+                return result.Match(Results.Ok, CustomResults.Problem);
             })
            . WithTags(Tags.Cases)
             ;
