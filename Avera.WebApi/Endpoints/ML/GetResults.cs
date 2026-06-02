f5c3f114445550570b05fab83f84d2ef4b737b82
recca383 <sirpatrick121402@gmail.com>
2026-06-02 14:48:47 +0800

    Add GetResults endpoint for ML analysis results retrieval and remove obsolete endpoints

---
+++ Avera.WebApi/Endpoints/ML/GetResults.cs
@@ -0,0 +1,32 @@
+
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.ML.GetResults;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+using SharedKernel;
+
+namespace Avera.WebApi.Endpoints.ML
+{
+    internal sealed class GetResults : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapGet("cases/{caseId:guid}/results", async (
+                Guid caseId,
+                ICommandHandler<GetMLResultsCommand, GetMLResultsResponse> handler,
+                CancellationToken cancellationToken
+            )=>
+            {
+                var command = new GetMLResultsCommand(caseId);
+
+                var result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(
+                    onSuccess => Results.Stream(result.Value.ResultsStream, "application/json"),
+                    onFailure: CustomResults.Problem);
+            })
+            .WithTags(Tags.ML)
+            .WithSummary("Get the results of an ML analysis for a case");
+        }
+    }
+}
