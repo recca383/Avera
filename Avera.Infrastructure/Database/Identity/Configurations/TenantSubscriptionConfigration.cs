6a0678a6749d4cdaff3222da837d2c0aa01ec54d
recca383 <sirpatrick121402@gmail.com>
2026-06-11 20:06:16 +0800

    changed Tenant subscription configuration

--- Avera.Infrastructure/Database/Identity/Configurations/TenantSubscriptionConfigration.cs
+++ Avera.Infrastructure/Database/Identity/Configurations/TenantSubscriptionConfigration.cs
@@ -1,6 +1,6 @@
-using Avera.Domain.Identity.SubscriptionPlans;
-using Avera.Domain.Identity.Tenants;
-using Avera.Domain.Identity.TenantSubscriptions;
+using Avera.Infrastructure.Identity.SubscriptionPlans;
+using Avera.Infrastructure.Identity.Tenants;
+using Avera.Infrastructure.Identity.TenantSubscriptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
