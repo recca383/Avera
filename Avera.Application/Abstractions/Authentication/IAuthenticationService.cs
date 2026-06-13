374e90dfcb77135ac0342d4db4e6cf96225f6b8d
recca383 <sirpatrick121402@gmail.com>
2026-06-13 20:17:07 +0800

    Added abstraction for authentication services

---
+++ Avera.Application/Abstractions/Authentication/IAuthenticationService.cs
@@ -0,0 +1,32 @@
+using Avera.Application.Authentication.Login;
+using Avera.Application.Authentication.Register;
+using SharedKernel;
+
+namespace Avera.Application.Abstractions.Authentication
+{
+    public interface IAuthenticationService
+    {
+         Task<Result<LoginResponse>> LoginAsync(
+            string email,
+            string password,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> RegisterAsync(
+            RegisterRequest request,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> LogoutAsync(
+            Guid userId,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> ForgotPasswordAsync(
+            string email,
+            CancellationToken cancellationToken = default);
+
+        Task<Result> ResetPasswordAsync(
+            Guid userId,
+            string token,
+            string password,
+            CancellationToken cancellationToken = default);
+    }
+}
