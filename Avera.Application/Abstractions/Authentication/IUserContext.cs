f97cdfbc8ebe8beac786520d702dcbe3cf514919
recca383 <sirpatrick121402@gmail.com>
2026-06-14 20:17:11 +0800

    updated iusercontext

--- Avera.Application/Abstractions/Authentication/IUserContext.cs
+++ Avera.Application/Abstractions/Authentication/IUserContext.cs
@@ -9,6 +9,8 @@ namespace Avera.Application.Abstractions.Authentication
     public interface IUserContext
     {
         Guid UserId { get; }
-        Guid TenantId { get; }
+        string Email { get; }
+        bool IsAuthenticated { get; }
+        IReadOnlyCollection<string> Roles { get; }
     }
 }
