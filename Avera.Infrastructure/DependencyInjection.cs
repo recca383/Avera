d3ab56df8f52fe57a04918a3b2400e1b5718b165
recca383 <sirpatrick121402@gmail.com>
2026-06-23 20:44:59 +0800

    Registered Authentication service and jwt service

--- Avera.Infrastructure/DependencyInjection.cs
+++ Avera.Infrastructure/DependencyInjection.cs
@@ -5,10 +5,14 @@ using Avera.Application.Abstractions.Storage;
 using Avera.Infrastructure.Authentication;
 using Avera.Infrastructure.Database.Application;
 using Avera.Infrastructure.Database.Identity;
+using Avera.Infrastructure.Identity.Roles;
+using Avera.Infrastructure.Identity.Users;
 using Avera.Infrastructure.ML;
 using Avera.Infrastructure.Storage;
 using Avera.Infrastructure.Time;
 using Infrastructure.DomainEvents;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.Configuration;
@@ -44,9 +48,23 @@ namespace Avera.Infrastructure
 
             services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
              options.UseNpgsql(applicationConnectionString));
-
-            services.AddDbContext<IApplicationIdentityDbContext, ApplicationIdentityDbContext>(options =>
+            
+            services.AddDbContext<IdentityDbContext>(options =>
              options.UseNpgsql(identityConnectionString));
+
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<IdentityDbContext>()
+            .AddDefaultTokenProviders();
+
 
             return services;
         }
@@ -58,6 +76,25 @@ namespace Avera.Infrastructure
 
         private static IServiceCollection AddAuthenticationInternal(this IServiceCollection services, IConfiguration configuration)
         {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    // {
+                    //     ValidateIssuer = true,
+                    //     ValidateAudience = true,
+                    //     ValidateLifetime = true,
+                    //     ValidateIssuerSigningKey = true,
+                    //     ValidIssuer = configuration["Jwt:Issuer"],
+                    //     ValidAudience = configuration["Jwt:Audience"],
+                    //     IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT Key is not configured.")))
+                    // };
+                });
+
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<JwtProvider>();
+
             services.AddHttpContextAccessor();
             services.AddScoped<IUserContext, UserContext>();
 
@@ -66,6 +103,7 @@ namespace Avera.Infrastructure
 
         private static IServiceCollection AddAuthorizationInternal(this IServiceCollection services)
         { 
+            services.AddAuthorization();
             return services;
         }
     }
