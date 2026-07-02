14b66e13f2d95fa1f910f2a3b17ca92c86f83d1d
recca383 <sirpatrick121402@gmail.com>
2026-07-02 18:13:15 +0800

    Added Role Configuration

---
+++ Avera.Infrastructure/Database/Identity/Configurations/RoleConfiguration.cs
@@ -0,0 +1,35 @@
+using Avera.Infrastructure.Identity.Roles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Avera.Infrastructure.Database.Identity.Configurations
+{
+    internal sealed class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            var adminRoleId =
+            Guid.Parse("11111111-1111-1111-1111-111111111111");
+
+            var userRoleId =
+            Guid.Parse("22222222-2222-2222-2222-222222222222");
+
+            builder.HasData(
+                new Role
+                {
+                    Id = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = adminRoleId.ToString()
+                },
+                new Role
+                {
+                    Id = userRoleId,
+                    Name = "User",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = userRoleId.ToString()
+                }
+            );
+        }
+    }
+}
