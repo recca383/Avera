331c7b51f55f5573ecddaa22adb4a38b199f597d
recca383 <sirpatrick121402@gmail.com>
2026-07-06 18:14:15 +0800

    Added bearer transformer to scalar

---
+++ Avera.WebApi/Infrastructure/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+public class BearerSecuritySchemeTransformer : IOpenApiDocumentTransformer
+{
+    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        document.Components ??= new();
+
+        document.Components.SecuritySchemes["Bearer"] =
+            new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT"
+            };
+
+        return Task.CompletedTask;
+    }
+}
