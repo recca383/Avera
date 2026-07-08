000181f76fcd9d8d64e2b0b7675e799cdcb27daf
recca383 <sirpatrick121402@gmail.com>
2026-07-08 18:15:33 +0800

    Added forgot password endpoint

--- Avera.WebApi/Endpoints/Auth/ForgotPassword.cs
+++ Avera.WebApi/Endpoints/Auth/ForgotPassword.cs
@@ -1,15 +1,25 @@
 ﻿
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Authentication.ForgotPassword;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+using SharedKernel;
+
 namespace Avera.WebApi.Endpoints.Auth
 {
     internal sealed class ForgotPassword : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPost("/auth/forgot-password", (
-
+            routeBuilder.MapPost("/auth/forgot-password", async (
+                ForgotPasswordCommand command,
+                ICommandHandler<ForgotPasswordCommand> handler,
+                CancellationToken cancellationToken
                 ) =>
             {
-                
+                var result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
             })
            . WithTags(Tags.Auth)
            ;
