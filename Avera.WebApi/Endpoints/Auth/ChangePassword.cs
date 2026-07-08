338b330ecf5796f2ecdcde823e562caf3cf992d8
recca383 <sirpatrick121402@gmail.com>
2026-07-08 18:15:17 +0800

    Added ChangePassword endpoint

---
+++ Avera.WebApi/Endpoints/Auth/ChangePassword.cs
@@ -0,0 +1,36 @@
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Authentication.ChangePassword;
+using Avera.Infrastructure.Identity.Roles;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+using SharedKernel;
+
+namespace Avera.WebApi.Endpoints.Auth
+{
+    public sealed class ChangePassword : IEndpoint
+    {
+        private record Request
+        (
+            string NewPassword,
+            string CurrentPassword
+        );
+
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapPost("auth/change-password", async (
+                Request request,
+                ICommandHandler<ChangePasswordCommand> handler,
+                CancellationToken cancellationToken
+            ) =>
+            {
+                var command = new ChangePasswordCommand(request.NewPassword, request.CurrentPassword);
+
+                var result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .RequireAuthorization()
+            .WithTags(Tags.Auth);
+        }
+    }
+}
