d01dfa176d38604df70e28d3726b42436148491c
recca383 <sirpatrick121402@gmail.com>
2026-07-09 18:15:18 +0800

    Added login endpoint

--- Avera.WebApi/Endpoints/Auth/Login.cs
+++ Avera.WebApi/Endpoints/Auth/Login.cs
@@ -1,13 +1,24 @@
 ﻿
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Authentication.Login;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
 namespace Avera.WebApi.Endpoints.Auth
 {
     internal sealed class Login : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPost("/auth/login", () =>
+            routeBuilder.MapPost("/auth/login", async (
+                LoginCommand command,
+                ICommandHandler<LoginCommand, LoginResponse> handler,
+                CancellationToken cancellationToken
+            ) =>
             {
+                var result = await handler.Handle(command, cancellationToken);
 
+                return result.Match(Results.Ok, CustomResults.Problem);
             })
            . WithTags(Tags.Auth)
             ;
