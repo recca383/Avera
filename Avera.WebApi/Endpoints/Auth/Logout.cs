644fec3d7023a25851a74fbce8b9646891795cff
recca383 <sirpatrick121402@gmail.com>
2026-07-10 18:15:07 +0800

    Added logout endpoint

--- Avera.WebApi/Endpoints/Auth/Logout.cs
+++ Avera.WebApi/Endpoints/Auth/Logout.cs
@@ -1,16 +1,27 @@
 ﻿
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Authentication.Logout;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
 namespace Avera.WebApi.Endpoints.Auth
 {
     internal sealed class Logout : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPost("/auth/logout", () =>
+            routeBuilder.MapPost("/auth/logout", async (
+                LogoutCommand command,
+                ICommandHandler<LogoutCommand> handler,
+                CancellationToken cancellationToken
+            ) =>
             {
+                var result = await handler.Handle(command, cancellationToken);
 
+                return result.Match(Results.NoContent, CustomResults.Problem);
             })
-           . WithTags(Tags.Auth)
-            ;
+           .WithTags(Tags.Auth)
+           .RequireAuthorization();
         }
     }
 }
