75fa9bbe81bdf045abaec7ce6d092a74d66511d5
recca383 <sirpatrick121402@gmail.com>
2026-07-10 18:15:17 +0800

    Added Register Endpoint

--- Avera.WebApi/Endpoints/Auth/Register.cs
+++ Avera.WebApi/Endpoints/Auth/Register.cs
@@ -1,13 +1,24 @@
 ﻿
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Authentication.Register;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
 namespace Avera.WebApi.Endpoints.Auth
 {
     internal sealed class Register : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPost("/auth/register", () =>
+            routeBuilder.MapPost("/auth/register", async (
+                RegisterCommand command,
+                ICommandHandler<RegisterCommand> handler,
+                CancellationToken cancellationToken
+            ) =>
             {
+                var result = await handler.Handle(command, cancellationToken);
 
+                return result.Match(Results.NoContent, CustomResults.Problem);
             })
            . WithTags(Tags.Auth)
             ;
