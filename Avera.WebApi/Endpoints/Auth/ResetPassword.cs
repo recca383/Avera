1d6b960187cd1e7bea56218c7149f2dbcb4db0a7
recca383 <sirpatrick121402@gmail.com>
2026-07-10 18:15:28 +0800

    Added ResetPassword endpoint

--- Avera.WebApi/Endpoints/Auth/ResetPassword.cs
+++ Avera.WebApi/Endpoints/Auth/ResetPassword.cs
@@ -1,13 +1,24 @@
 ﻿
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Authentication.ResetPassword;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
 namespace Avera.WebApi.Endpoints.Auth
 {
     internal sealed class ResetPassword : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPost("/auth/reset-password", () =>
+            routeBuilder.MapPost("/auth/reset-password", async (
+                ResetPasswordCommand command,
+                ICommandHandler<ResetPasswordCommand> handler,
+                CancellationToken cancellationToken
+            ) =>
             {
+                var result = await handler.Handle(command, cancellationToken);
 
+                return result.Match(Results.NoContent, CustomResults.Problem);
             })
            . WithTags(Tags.Auth)
             ;
