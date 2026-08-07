f5834b5bc070c894f8ed2068b62e48499854b208
recca383 <sirpatrick121402@gmail.com>
2026-08-07 13:17:42 +0800

    Add MemberRequest and MemberRequestStatus classes for handling member requests

---
+++ Avera.Domain/Identity/MemberRequests/MemberRequest.cs
@@ -0,0 +1,21 @@
+using Avera.Domain.Identity.Tenants;
+using Avera.Domain.Identity.Users;
+
+namespace Avera.Domain.Identity.MemberRequests
+{
+    public class MemberRequest
+    {
+         public Guid Id { get; set; }
+         public MemberRequestStatus Status { get; set; }
+         public DateTime CreatedAt { get; set; }
+         public DateTime ReviewedAt { get; set; }
+         public Guid ReviewedByUserId { get; set; }
+
+         // Navigation Attributes
+
+         public User? User { get; set; }
+         public Guid UserId { get; set; }
+        public Tenant? Tenant { get; set; }
+         public Guid TenantId { get; set; }
+    }
+}
---
+++ Avera.Domain/Identity/MemberRequests/Status.cs
@@ -0,0 +1,10 @@
+namespace Avera.Domain.Identity.MemberRequests
+{
+    public enum MemberRequestStatus
+    {
+         Pending,
+         Approved,
+         Rejected,
+         Cancelled
+    }
+}
