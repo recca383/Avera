77a85e7a387eb99026a4c7deccb797ba315ccef5
recca383 <sirpatrick121402@gmail.com>
2026-09-04 02:22:24 +0800

    Created Remove User command

---
+++ Avera.Application/Tenants/RemoveUser/RemoveUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.RemoveFromTenant
+{
+    public sealed record RemoveUserCommand(Guid userId) : ICommand;
+}
---
+++ Avera.Application/Tenants/RemoveUser/RemoveUserCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Avera.Application.Abstractions.Authentication;
+using Avera.Application.Abstractions.Messaging;
+using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.RemoveFromTenant
+{
+    internal sealed class RemoveUserCommandHandler(
+        IAdminService adminService) : ICommandHandler<RemoveUserCommand>
+    {
+        public async Task<Result> Handle(RemoveUserCommand command, CancellationToken cancellationToken)
+        {
+            return await adminService.RemoveUserAsync(command.userId, cancellationToken);
+        }
+    }
+}
