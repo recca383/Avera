da2e87fef3ff8f6474e1f648505aa45beb73dbe3
recca383 <sirpatrick121402@gmail.com>
2026-09-04 02:22:43 +0800

    Created Suspend user command

---
+++ Avera.Application/Tenants/SuspendUser/SuspendUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.SuspendUser
+{
+    public sealed record SuspendUserCommand(Guid UserId) : ICommand;
+}
---
+++ Avera.Application/Tenants/SuspendUser/SuspendUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Avera.Application.Abstractions.Authentication;
+using Avera.Application.Abstractions.Messaging;
+using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.SuspendUser
+{
+    internal sealed class SuspendUserCommandHandler(IAdminService adminService) : ICommandHandler<SuspendUserCommand>
+    {
+        public async Task<Result> Handle(SuspendUserCommand command, CancellationToken cancellationToken)
+        {
+            return await adminService.SuspendUserAsync(command.UserId, cancellationToken);
+        }
+    }
+}
