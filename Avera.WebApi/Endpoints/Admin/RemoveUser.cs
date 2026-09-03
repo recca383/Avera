2eb83a0871b0c9c7a71ee6c0d008d52f97c92f35
recca383 <sirpatrick121402@gmail.com>
2026-09-04 02:25:27 +0800

    Created Remove User and suspend user endpoints

---
+++ Avera.WebApi/Endpoints/Admin/RemoveUser.cs
@@ -0,0 +1,28 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Tenants.RemoveFromTenant;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+using Npgsql.Replication;
+
+namespace Avera.WebApi.Endpoints.Admin
+{
+    internal sealed class RemoveUser : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapDelete("/tenant/members/{userId:guid}", async (
+                Guid userId,
+                ICommandHandler<RemoveUserCommand> handler,
+                CancellationToken cancellationToken) =>
+            {
+                var command = new RemoveUserCommand(userId);
+
+                var result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            })
+            .RequireAuthorization(RolePolicy.Admin)
+            .WithTags(Tags.OrgAdmin);
+        }
+    }
+}
---
+++ Avera.WebApi/Endpoints/Admin/SuspendUser.cs
@@ -0,0 +1,24 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Tenants.SuspendUser;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
+namespace Avera.WebApi.Endpoints.Admin
+{
+    internal sealed class SuspendUser : IEndpoint
+    {
+        void IEndpoint.MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapPost("/admin/suspend-user", async (
+                Guid UserId,
+                ICommandHandler<SuspendUserCommand> handler,
+                CancellationToken cancellationToken
+                ) =>
+            {
+                var result = await handler.Handle(new SuspendUserCommand(UserId), cancellationToken);
+
+                return result.Match(Results.NoContent, CustomResults.Problem);
+            });
+        }
+    }
+}
