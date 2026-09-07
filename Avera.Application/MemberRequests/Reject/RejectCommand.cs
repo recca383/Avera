09b6601cf043b989435a61a9f444bb5dc328bc06
recca383 <sirpatrick121402@gmail.com>
2026-09-07 21:18:01 +0800

    Add RejectCommand and handler for member request rejection
    Implemented RejectCommand and RejectCommandHandler to handle member request rejection by ID, including validation, database updates, and sending rejection emails to users.

---
+++ Avera.Application/MemberRequests/Reject/RejectCommand.cs
@@ -0,0 +1,9 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.MemberRequests.Reject
+{
+    public sealed record RejectCommand(Guid MemberRequestId) : ICommand;
+}
---
+++ Avera.Application/MemberRequests/Reject/RejectCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using Avera.Application.Abstractions.Authentication;
+using Avera.Application.Abstractions.Databases;
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Abstractions.Services;
+using Avera.Domain.Identity.MemberRequests;
+using Avera.Domain.Identity.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Avera.Application.MemberRequests.Reject
+{
+    internal sealed class RejectCommandHandler
+        (
+            IIdentityDbContext identityDbContext,
+            IUserContext userContext,
+            UserManager<User> userManager,
+            IEmailService emailService
+        ) : ICommandHandler<RejectCommand>
+    {
+        public async Task<Result> Handle(RejectCommand command, CancellationToken cancellationToken)
+        {
+            var request = await identityDbContext.MemberRequests
+               .FindAsync(new object[] { command.MemberRequestId }, cancellationToken);
+
+            if (request == null)
+            {
+                return Result.Failure(MemberRequestErrors.MemberRequestNotFound);
+            }
+
+            var reviewedByUserId = userContext.UserId;
+
+            request.Reject(reviewedByUserId);
+
+            var user = await userManager.FindByIdAsync(request.UserId.ToString());
+
+            if (user == null)
+            {
+                return Result.Failure(UserErrors.UserNotFound);
+            }
+
+            await identityDbContext.SaveChangesAsync(cancellationToken);
+
+            await emailService.SendRequestRejectedAsync(user.Email!, cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
