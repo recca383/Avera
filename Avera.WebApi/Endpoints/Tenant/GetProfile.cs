9225a6f6eee43a13e5e0d5f967db628adcc5ce5a
recca383 <sirpatrick121402@gmail.com>
2026-09-10 16:26:41 +0800

    implemented get profile and updated test email

---
+++ Avera.WebApi/Endpoints/Tenant/GetProfile.cs
@@ -0,0 +1,30 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Tenants.GetProfile;
+using Avera.Domain.Identity.Tenants;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
+namespace Avera.WebApi.Endpoints.Tenant
+{
+    internal sealed class GetProfile : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapGet("/tenant/profile", async (
+                IQueryHandler<GetTenantProfileQuery, TenantProfileDto> handler,
+                CancellationToken cancellationToken
+            ) =>
+            {
+                var result = await handler.Handle(
+                    new GetTenantProfileQuery(),
+                    cancellationToken);
+
+                return result.Match(
+                    Results.Ok,
+                    CustomResults.Problem);
+            })
+            .RequireAuthorization()
+            .WithTags("Tenant");
+        }
+    }
+}
--- Avera.WebApi/Endpoints/TestEmail.cs
+++ Avera.WebApi/Endpoints/TestEmail.cs
@@ -1,4 +1,5 @@
 using Avera.Application.Abstractions.Services;
+using Avera.Domain.Identity.Users;
 using Microsoft.AspNetCore.Identity;
 
 namespace Avera.WebApi.Endpoints
@@ -7,11 +8,41 @@ namespace Avera.WebApi.Endpoints
     {
         public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
         {
-            routeBuilder.MapPost("test", async (IEmailService service, CancellationToken cancellationToken)=>
+            routeBuilder.MapPost("test", async (IEmailService service, UserManager<User> _userManager, CancellationToken cancellationToken) =>
             {
-               // return configuration["ApplicationDbConnectionString"] + "\n\n" + configuration["ApplicationIdentityDbConnectionString"];
-                await service.SendRequestRejectedAsync("sirpatrick121402@gmail.com", cancellationToken);
-            }).RequireAuthorization();
+                var testEmail = "sirpatrick121402@gmail.com";
+                var user = _userManager.FindByEmailAsync(testEmail).Result;
+
+                var verificationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user!);
+
+                var newEmail = "patrickfernandez.dev@gmail.com";
+                var changeEmailToken = await _userManager.GenerateChangeEmailTokenAsync(user!, newEmail);
+
+                var appUrl = "avera://";
+
+
+                var date = DateOnly.FromDateTime(DateTime.UtcNow);
+
+                var time = TimeOnly.FromDateTime(DateTime.UtcNow);
+
+                // return configuration["ApplicationDbConnectionString"] + "\n\n" + configuration["ApplicationIdentityDbConnectionString"];
+
+                await service.SendEmailVerificationAsync(user.Email, user.FirstName,
+                $"{verificationToken}", cancellationToken);
+
+                await service.SendEmailChangeVerificationAsync(user.Email, user.FirstName,
+                $"{changeEmailToken}", cancellationToken);
+
+                await service.SendEmailNotificationToNewEmail(user.Email, user.FirstName,
+                date, time, appUrl, cancellationToken);
+
+                await service.SendEmailVerified(user.Email, user.FirstName,
+                appUrl, cancellationToken);
+
+                await service.SendEmailNotificationToOldEmail(user.Email, user.FirstName, newEmail, date, time);
+
+                
+            });
             
         }
     }
