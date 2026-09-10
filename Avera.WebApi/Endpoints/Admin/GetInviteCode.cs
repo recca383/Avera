03f189b56e3ed4c056298491ba91456b5d1d88fd
recca383 <sirpatrick121402@gmail.com>
2026-09-10 16:22:47 +0800

    implemented Get invite code endpoint

---
+++ Avera.WebApi/Endpoints/Admin/GetInviteCode.cs
@@ -0,0 +1,32 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Tenants.GetInviteCode;
+using Avera.Domain.Identity.Tenants;
+using Avera.WebApi.Extensions;
+using Avera.WebApi.Infrastructure;
+
+namespace Avera.WebApi.Endpoints.Admin
+{
+    internal sealed class GetInviteCode : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapGet("/tenant/invite-code",
+                async (
+                    IQueryHandler<
+                        GetTenantInviteCodeQuery,
+                        TenantInviteCodeDto> handler,
+                    CancellationToken cancellationToken) =>
+                {
+                    var result = await handler.Handle(
+                        new GetTenantInviteCodeQuery(),
+                        cancellationToken);
+
+                    return result.Match(
+                        Results.Ok,
+                        CustomResults.Problem);
+                })
+                .RequireAuthorization(RolePolicy.Admin)
+                .WithTags(Tags.OrgAdmin);
+        }
+    }
+}
