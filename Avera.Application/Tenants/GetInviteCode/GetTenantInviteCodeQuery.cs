3e9a6c3ae05742d0cd04378b2b48ab8681d1720b
recca383 <sirpatrick121402@gmail.com>
2026-09-10 16:20:53 +0800

    Added get tenant invite code

---
+++ Avera.Application/Tenants/GetInviteCode/GetTenantInviteCodeQuery.cs
@@ -0,0 +1,10 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using Avera.Domain.Identity.Tenants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.GetInviteCode
+{
+    public sealed record GetTenantInviteCodeQuery : IQuery<TenantInviteCodeDto>;
+}
---
+++ Avera.Application/Tenants/GetInviteCode/GetTenantInviteCodeQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Avera.Application.Abstractions.Authentication;
+using Avera.Application.Abstractions.Databases;
+using Avera.Application.Abstractions.Messaging;
+using Avera.Application.Tenants.GetInviteCode;
+using Microsoft.EntityFrameworkCore;
+using Avera.Domain.Identity.Tenants;
+using Avera.Domain.Identity.Users;
+using SharedKernel;
+
+internal sealed class GetTenantInviteCodeQueryHandler(
+    IIdentityDbContext db,
+    IUserContext userContext
+) : IQueryHandler<GetTenantInviteCodeQuery, TenantInviteCodeDto>
+{
+    public async Task<Result<TenantInviteCodeDto>> Handle(
+        GetTenantInviteCodeQuery query,
+        CancellationToken cancellationToken)
+    {
+        var tenantId = userContext.TenantId;
+
+        if (!tenantId.HasValue)
+        {
+            return Result.Failure<TenantInviteCodeDto>(
+                TenantErrors.NotMember);
+        }
+
+        var inviteCode = await db.Tenants
+            .Where(t => t.Id == tenantId.Value)
+            .Select(t => t.InviteCode)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (inviteCode is null)
+        {
+            return Result.Failure<TenantInviteCodeDto>(
+                UserErrors.TenantNotFound);
+        }
+
+        return Result.Success(
+            new TenantInviteCodeDto(inviteCode));
+    }
+}
