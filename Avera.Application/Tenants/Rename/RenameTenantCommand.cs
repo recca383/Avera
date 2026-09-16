245ffcd999526321a44ba098dbee479ff853e1d0
recca383 <sirpatrick121402@gmail.com>
2026-09-16 15:48:15 +0800

    Add RenameTenantCommand and handler for renaming org
    Two new files were added: RenameTenantCommand.cs, defining a command record with a newName property, and RenameTenantCommandHandler.cs, implementing the handler that uses IAdminService to rename the organization.

---
+++ Avera.Application/Tenants/Rename/RenameTenantCommand.cs
@@ -0,0 +1,11 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.Rename
+{
+    public sealed record RenameTenantCommand(
+        string newName
+        ) : ICommand;
+}
---
+++ Avera.Application/Tenants/Rename/RenameTenantCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Avera.Application.Abstractions.Authentication;
+using Avera.Application.Abstractions.Messaging;
+using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.Tenants.Rename
+{
+    internal sealed class RenameTenantCommandHandler(IAdminService adminService) : ICommandHandler<RenameTenantCommand>
+    {
+        public async Task<Result> Handle(RenameTenantCommand command, CancellationToken cancellationToken)
+        {
+            return await adminService.RenameOrganization(command.newName, cancellationToken);
+        }
+    }
+}
