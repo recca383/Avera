24074affc889b274a294ac1b2234af29f2fe5e96
recca383 <sirpatrick121402@gmail.com>
2026-09-16 15:48:03 +0800

    Add CancelMemberRequest command and handler
    Implemented CancelMemberRequestCommand and its handler to allow users to cancel their pending member requests. The handler updates the request status to Cancelled and persists changes to the database, returning a result based on the operation's success.

---
+++ Avera.Application/MemberRequests/Cancel/CancelMemberRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using Avera.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.MemberRequests.Cancel
+{
+    public sealed record class CancelMemberRequestCommand : ICommand;
+}
---
+++ Avera.Application/MemberRequests/Cancel/CancelMemberRequestCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Avera.Application.Abstractions.Authentication;
+using Avera.Application.Abstractions.Databases;
+using Avera.Application.Abstractions.Messaging;
+using Avera.Domain.Identity.MemberRequests;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avera.Application.MemberRequests.Cancel
+{
+    internal sealed class CancelMemberRequestCommandHandler(
+        IIdentityDbContext identityDbContext,
+        IUserContext userContext
+        ) : ICommandHandler<CancelMemberRequestCommand>
+    {
+        public async Task<Result> Handle(CancelMemberRequestCommand command, CancellationToken cancellationToken)
+        {
+            var pendingMemberRequest = await identityDbContext
+                .MemberRequests
+                .SingleOrDefaultAsync(m => m.UserId == userContext.UserId 
+                                        && m.Status == MemberRequestStatus.Pending, 
+                                           cancellationToken);
+            
+            if (pendingMemberRequest == null)
+            {
+                return Result.Failure(MemberRequestErrors.MemberRequestNotFound);
+            }
+
+            pendingMemberRequest.Status = MemberRequestStatus.Cancelled;
+
+            await identityDbContext.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
